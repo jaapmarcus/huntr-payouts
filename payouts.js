58fe97f85f6008c1199a4fa6aafb928106a7a428
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-10-21 15:48:58 +0200

    Get reward bonus from repo and display it
    + Add fraction to dollar conversion

--- payouts.js
+++ payouts.js
@@ -1,9 +1,37 @@
+// API key 
+var apiKey=""
+// GraphQL database
+var graphQL=""
+
+if (apiKey == '' || graphQL==''){
+  throw new Error("Please provide API key and GraphQL url first");
+}
+
+function fractionToDollars(t){
+  switch (t) {
+    case 0:
+        return "";
+    case .1:
+        return "$";
+    case .3:
+        return "$$";
+    case .5:
+        return "$$$";
+    case .8:
+        return "$$$$";
+    case 1:
+        return "$$$$$";
+    default:
+        return ""
+    }
+}
+
 function fetchData() {
-    return fetch("https://heretheurlforgraphqlhuntruses", {
+    return fetch(graphQL, {
         method: "POST",
         headers: {
             "content-type": "application/json",
-            "x-api-key": "api-key"
+            "x-api-key": apiKey
         },
         body: JSON.stringify({
            query: /* GraphQL */ `
@@ -15,20 +43,61 @@ function fetchData() {
     })
     .then(response => response.json()).then(data => buildTable(data.data.query.items))
 };
-test = fetchData();
+fetchData();
 
-
+function fetchReward(repo){
+  const [ owner, name] = repo.split('/');
+  fetch(graphQL, {
+    method: "POST",
+    headers: {
+        "x-api-key": apiKey
+    },
+    body: JSON.stringify({
+        query: /* GraphQL */ `
+        query GetBountyMetrics($owner: String!, $name: String!) {
+            getBountyMetrics(owner: $owner, name: $name) {
+            bounty_reward
+            popularity_score
+            merge_chance
+            }
+        }`,
+        variables: {
+            owner,
+            name
+        }
+    }),
+})
+.then(response => response.json())
+.then(({ data }) => buildTable(cwe, data.getBountyMetrics.bounty_reward));
+}
 
 function sortFunction(a, b) {
  return a['number'] - b['number'];
 }
-function buildTable(data){
+
+var cwe = '';
+function buildTable(data, reward = 0){
+  cwe = data  
   data.sort(sortFunction);
+  rows = document.getElementById('rows');
+  rows.innerHTML='';
   for (i = 0; i < data.length; i++){
       item = data[i];
       tr = document.createElement('tr');
-      tr.innerHTML="<td>"+item.number+"</td><td>"+item.title+"</td><td>"+item.description+"</td><td>" + item.pricing_multiplier + "</tr>";
-      rows = document.getElementById('rows');
+      tr.innerHTML="<td>"+item.number+"</td><td>"+item.title+"</td><td>"+item.description+"</td><td>" + item.pricing_multiplier +"</td><td>" + fractionToDollars(item.pricing_multiplier)+"</td><td>"+reward * item.pricing_multiplier +"</td><td>"+ reward * item.pricing_multiplier * 0.25+"</td>";
       rows.appendChild(tr);
   }
+}
+
+var readyStateCheckInterval = setInterval(function() {
+  if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
+      document.getElementById('query').addEventListener('submit', submitForm);
+  }
+}, 10);
+
+function submitForm(e){
+  e.preventDefault();
+  fetchReward(document.getElementById('repo').value)
+  return false;
 }
